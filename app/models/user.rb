b4da5b1deb4a375f9a5efd00d470764ca49bffb4
chris <christanr00@gmail.com>
2023-06-04 17:10:49 -0400

    Set up user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :gender, presence: true
+
+  enum :gender, { male: 0, female: 1 }
+end
