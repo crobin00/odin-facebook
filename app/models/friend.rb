d03e9b9e8297bc9defe681f0906eda1924335b57
chris <christanr00@gmail.com>
2023-06-12 21:34:23 -0400

    Fix some friend styling

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,7 @@
 class Friend < ApplicationRecord
   validates :user_id, presence: true
   validates :friend_id, presence: true
+  validates_uniqueness_of :user_id, :scope => [:friend_id]
 
   belongs_to :user
   belongs_to :friend, class_name: "User"
