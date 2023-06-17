3bcff4157259c7c98076ec19deb32cb1cb74de80
chris <christanr00@gmail.com>
2023-06-16 23:41:43 -0400

    Allow user uploaded profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 require 'digest/md5'
+require 'open-uri'
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -9,7 +10,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
   validates :gender, presence: true
-  before_create :create_profile_url
+  validate :acceptable_photo
+  before_create :create_profile_picture
   after_create :send_welcome_email
 
   enum :gender, { male: 0, female: 1 }
@@ -20,10 +22,10 @@ class User < ApplicationRecord
   has_many :requests, class_name: "FriendRequest", foreign_key: "sent_user_id", dependent: :destroy
   has_many :friendships, class_name: "Friend", foreign_key: "user_id", dependent: :destroy
   has_many :friends, through: :friendships, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
 
-  def create_profile_url
-    emailHash = Digest::MD5.hexdigest(self.email)
-    self.profile_pic_url = "https://www.gravatar.com/avatar/#{emailHash}?s=56" if self.profile_pic_url.nil?
+  def create_profile_picture
+    self.avatar.attach(io: File.open("app/assets/images/default_profile_picture.jpg"), filename: "default_profile_picture.jpg", content_type: "image/jpeg") unless self.avatar.attached?
   end
 
   def remove_friend(friend)
@@ -37,7 +39,8 @@ class User < ApplicationRecord
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.gender = (auth.extra.raw_info.gender == "male" || auth.extra.raw_info.gender == "female") ? auth.extra.raw_info.gender : "male"
-      user.profile_pic_url = auth.info.image 
+      facebook_image_url = URI.open(auth.info.image)
+      user.avatar.attach(io: facebook_image_url, filename: "facebook_image.jpg")
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
@@ -46,5 +49,14 @@ class User < ApplicationRecord
 
   def send_welcome_email
     UserMailer.with(user: self).welcome_email.deliver_later
+  end
+
+  def acceptable_photo
+    return unless avatar.attached?
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(avatar.content_type)
+      errors.add(:avatar, "must be a JPEG OR PNG")
+    end
   end
 end
